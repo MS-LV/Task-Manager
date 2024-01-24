7de2645c342444375afd438e5b42b2d7896d41fb
jamoliddin <jamoliddinqodirov18@gmail.com>
2024-01-25 00:34:38 +0700

    first commit: create and connecting tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="app">
+</div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            
+        }
+    }, 
+    methods: {
+
+    },
+    components: {
+    
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
