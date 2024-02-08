5d550e07ca006fe5a88ef04f840112a7bc2aa934
Jamoliddin <jamoliddinqodirov18@gmail.com>
2024-02-08 16:34:34 +0500

    first  init before start devlop calculator

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <script setup>
-import TodoHeader from './components/TodoHeader.vue';
-import TodoList from './components/TodoList.vue';
 </script>
 
 <template>
-  <div class="flex flex-col w-screen min-h-screen space-y-4 max-w-screen-lg mx-auto text-white">
-    <TodoHeader></TodoHeader>
-    <TodoList></TodoList>
-  </div>
 </template>
 
-<style scope>
+<style>
   
 </style>
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 module.exports = {
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => {
-            return tag.startsWith('Todo') // (return true)
-          }
-        }
-      }
-    })
+    vue({})
   ],
 }
